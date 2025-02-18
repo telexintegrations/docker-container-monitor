9515927bbeba53556b35cd0ce368d91e1322f1d5
David Enikuomehin <enikuauth@gmail.com>
2025-02-18 23:36:16 +0100

    feat: create main entrypoint for fastApi application

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/healthcheck")
+async def health_check():
+    """Checks if server is active."""
+    return {"status": "active"}
